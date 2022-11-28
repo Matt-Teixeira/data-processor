da54e3a3d58d138ca6f38764b43a7a62b6cad4aa
JonathanPopeAvanteHS <jonathan.pope@avantehs.com>
2022-11-28 20:42:11 +0000

    Added basic redis example

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 ('use strict');
 require('dotenv').config();
 const { log } = require('./logger');
+const redis = require('redis');
 
 const onBoot = async () => {
    log('info', 'NA', 'NA', 'onBoot', `FN CALL`, {
@@ -11,22 +12,39 @@ const onBoot = async () => {
    });
 
    // SETUP ENV BASED RESOURCES -> REDIS CLIENT, JOB SCHEDULES
-   // const clienConfig = {
-   //    socket: {
-   //       port: 6379,
-   //       host: process.env.REDIS_IP,
-   //    },
-   // };
-   // const redisClient = redis.createClient(clienConfig);
+   const clienConfig = {
+      socket: {
+         port: 6379,
+         host: process.env.REDIS_IP,
+      },
+   };
+   const redisClient = redis.createClient(clienConfig);
 
-   // redisClient.on(
-   //    'error',
-   //    async (err) =>
-   //       await log('error', 'NA', 'NA', 'redisClient', `ON ERROR`, {
-   //          // TODO: KILL APP?
-   //          error: err,
-   //       })
-   // );
-   // await redisClient.connect();
+   redisClient.on(
+      'error',
+      async (err) =>
+         await log('error', 'NA', 'NA', 'redisClient', `ON ERROR`, {
+            // TODO: KILL APP?
+            error: err,
+         })
+   );
+   await redisClient.connect();
+
+   // REDIS I/O EXAMPLE
+   try {
+      // WRITE
+      const key = `SME123_temp`;
+      const value = '32F';
+      await redisClient.set(key, value);
+      // READ
+      const reading = await redisClient.get(key);
+      await log('info', 'uuid', 'sme', 'redisClient', 'FN DETAILS', {
+         reading: reading,
+      });
+   } catch (err) {
+      await log('error', 'uuid', 'sme', 'redisClient', 'FN CATCH', {
+         error: err,
+      });
+   }
 };
 onBoot();
